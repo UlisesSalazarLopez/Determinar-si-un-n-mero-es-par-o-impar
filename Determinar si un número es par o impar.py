f0a3e6503e52d4406bd894373403bcb6cee1278c
Ulises Salazar Lopez <victor.salazarlopez@cesunbc.edu.mx>
2024-09-15 17:41:05 -0700

    Add files via upload

---
+++ Determinar si un número es par o impar.py
@@ -0,0 +1,7 @@
+def es_par_o_impar():
+    numero = int(input("Introduce un número: "))
+    if numero % 2 == 0:
+        print(f"El número {numero} es par.")
+    else:
+        print(f"El número {numero} es impar.")
+es_par_o_impar()
